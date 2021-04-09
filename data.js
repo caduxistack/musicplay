e8b4a2f65fe8412d70734fe50da8dfba229a3a08
caduxistack <caduxis.tech@gmail.com>
2021-04-09 00:05:18 -0300

    initial commit

---
+++ data.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    title:
+      "01. Blodörn",
+    artist: "Månegarm",
+    cover: "01.png",
+    file: "01. Blodörn.mp3",
+  },
+  {
+    title: "04. Blot",
+    artist: "Månegarm",
+    cover: "04.png",
+    file: "04. Blot.mp3",
+  },
+  {
+    title: "12. Mother Earth, Father Thunder",
+    artist: "Månegarm",
+    cover: "12.png",
+    file: "12. Mother Earth, Father Thunder.mp3",
+  },
+  {
+    title: "13. Mother Earth, Father Thunder [Metal Version]",
+    artist: "Månegarm",
+    cover: "13.png",
+    file: "13. Mother Earth, Father Thunder [Metal Version].mp3",
+  },
+];
